8ded50ab452230d6b2d5bd806a6cc3a9ddd0ba19
xdshivani <shivani2499chauhan@gmail.com>
2020-09-20 12:37:37 +0530

    completed 1

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ jQuery(document).ready(function($) {
             e.preventDefault();
         }
         
-        $('nav a, .main-btn a').click(function(e) {
+        $('nav a').click(function(e) {
             // When link clicked, find slide it points to
             var newslide = parseInt($(this).attr('href')[1]);
             // find how far it is from current slide
